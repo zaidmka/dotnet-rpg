9b0bf45670513926de15cbcce1a2b5bd165ca189
zaidmka <zaid.mka.90@gmail.com>
2023-05-06 23:02:24 +0300

    Add User Auth and Registeration

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+            
+        }
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            var response = new ServiceResponse<int>();
+            if (await UserExits(user.Username))
+            {
+                response.Sucess=false;
+                response.Message="User Already exits.";
+                return response;
+            }
+            CreatePasswordHash(password,out byte[] passwordHash,out byte[] passwordSalt);
+            user.PasswordHash=passwordHash;
+            user.PasswordSalt=passwordSalt;
+            _context.Users.Add(user);
+            await _context.AddRangeAsync();
+            response.Data= user.Id;
+            return(response);
+        }
+
+        public async Task<bool> UserExits(string username)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt=hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
