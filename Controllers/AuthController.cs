a900262d1b50fa8a0769bb0f9764d3779ff38a65
zaidmka <zaid.mka.90@gmail.com>
2023-06-30 18:16:46 +0300

    add skils

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -28,5 +28,16 @@ namespace dotnet_rpg.Controllers
             }
             return Ok(response);
         }
+        
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<int>>> Login (UserLoginDto request)
+        {
+            var response = await _authRepo.Login(request.Username,request.Password);
+            if(!response.Sucess)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
     }
 }
--- Data/AuthRepository.cs
+++ Data/AuthRepository.cs
@@ -14,9 +14,26 @@ namespace dotnet_rpg.Data
             _context = context;
             
         }
-        public Task<ServiceResponse<string>> Login(string username, string password)
+        public async Task<ServiceResponse<string>> Login(string username, string password)
         {
-            throw new NotImplementedException();
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users
+            .FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
+            if(user is null)
+            {
+                response.Sucess=false;
+                response.Message="Username not exits.";
+            }
+            else if (!VerifyPasswordHash(password,user.PasswordHash,user.PasswordSalt))
+            {
+                response.Sucess=false;
+                response.Message="Password is Wrong!";
+            }
+            else
+            {
+                response.Data = user.Id.ToString();
+            }
+            return response;
         }
 
         public async Task<ServiceResponse<int>> Register(User user, string password)
@@ -53,6 +70,15 @@ namespace dotnet_rpg.Data
             {
                 passwordSalt=hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash,byte[]passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
             }
         }
     }
