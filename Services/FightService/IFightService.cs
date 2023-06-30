2261853afb9df4bbb5828b893bd98ee4931c80ac
zaidmka <zaid.mka.90@gmail.com>
2023-06-30 23:47:19 +0300

    another comment

--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -16,7 +16,7 @@ namespace dotnet_rpg.Services.FightService
 
         Task<ServiceResponse<List<HighScoreDto>>> GitHighScore();
 
-        // Hello, this is a comment
+        // Hello, this is a comment asd
 
 
     }
