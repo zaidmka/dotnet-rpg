4f8978c8212dbc14448b0345c4cf360de26de8cc
zaidmka <zaid.mka.90@gmail.com>
2023-07-01 03:39:28 +0300

    enable swagger in production environment

--- Program.cs
+++ Program.cs
@@ -53,7 +53,7 @@ builder.Services.AddScoped<IFightService,FightService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
